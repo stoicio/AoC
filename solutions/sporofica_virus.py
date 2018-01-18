d111eae9dddeab61407fe992b2f68b6570fab4b6
stoicio <bhalakumaran.m@gmail.com>
2018-01-18 11:22:12 -0800

    Change day 22 to use complex plane for 2D grid

--- solutions/sporofica_virus.py
+++ solutions/sporofica_virus.py
@@ -1,7 +1,3 @@
-INFECTED = '#'
-CLEAN = '.'
-
-
 def get_state_from_file(input_file_path):
     all_lines = []
     with open(input_file_path) as input_file:
@@ -11,49 +7,45 @@ def get_state_from_file(input_file_path):
     total_lines = len(all_lines)
     center = (total_lines / 2, len(all_lines[0]) / 2)  # (row, col) => (x, y)
 
+    center = center[0] + center[1] * 1j
+
     infected_nodes = set()
     for row in range(total_lines):
         for col in range(len(all_lines[0])):
             if all_lines[row][col] == '#':
-                offset_row = center[0] - row
-                offset_col = col - center[1]
-                infected_nodes.add((offset_row, offset_col))
+                offset = (center.real - row) + (col - center.imag) * 1j
+                infected_nodes.add(offset)
     return infected_nodes
 
 
 def get_next_dir(curr_direction, next_direction):
-    row, col = curr_direction[0], curr_direction[1]
+    next_dir = 0j
     if next_direction == 'reverse':  # Flip the signs 
-        row *= -1
-        col *= -1
+        next_dir = curr_direction * -1
     else:
-        row, col = curr_direction[1], curr_direction[0]  # swap row and col
         if next_direction == 'left':
-            col *= -1  # if direction is left, flip the sign of col
+            next_dir = curr_direction * -1j  # if direction is left, multiply by -1j
         else:
-            row *= -1  # if direction is right, flip the sign of row
-    return (row, col)
+            next_dir = curr_direction * 1j  # if direction is right, multiply by 1j
+    return next_dir
 
 
 def solve_part_one(input_file_path):
     infected_nodes = get_state_from_file(input_file_path)
-    curr_node = (0, 0)  # start from center
-    curr_dir = (1, 0)   # start facing UP
+    curr_node = 0 + 0j  # start from center
+    curr_dir = 1 + 0j   # start facing UP
     infections = 0
     for i in range(0, 10000):
+        next_dir = curr_dir
         if curr_node in infected_nodes:
             next_dir = get_next_dir(curr_dir, 'right')
-            # print 'Step ', i + 1, 'In Infected Node, Cleaning  ', curr_node, 'moving right'
             infected_nodes.remove(curr_node)
-            curr_node = (curr_node[0] + next_dir[0], curr_node[1] + next_dir[1])
-            curr_dir = next_dir
         else:
             next_dir = get_next_dir(curr_dir, 'left')
-            # print 'Step ', i + 1, 'In Clean Node   , Infecting ', curr_node, 'moving left'
             infected_nodes.add(curr_node)
             infections += 1
-            curr_node = (curr_node[0] + next_dir[0], curr_node[1] + next_dir[1])
-            curr_dir = next_dir
+        curr_node = curr_node + next_dir
+        curr_dir = next_dir
     return infections
 
 
@@ -62,36 +54,28 @@ def solve_part_two(input_file_path):
     weak_nodes = set()
     flagged_nodes = set()
 
-    curr_node = (0, 0)  # start from center
-    curr_dir = (1, 0)   # start facing UP
+    curr_node = 0 + 0j  # start from center
+    curr_dir = 1 + 0j   # start facing UP
 
     infections = 0
     for i in xrange(0, 10000000):
+        next_dir = curr_dir
         if curr_node in infected_nodes:
             next_dir = get_next_dir(curr_dir, 'right')
-            # print 'Step ', i + 1, 'In Infected Node, Flagging  ', curr_node, 'moving right'
             infected_nodes.remove(curr_node)
             flagged_nodes.add(curr_node)
-            curr_node = (curr_node[0] + next_dir[0], curr_node[1] + next_dir[1])
-            curr_dir = next_dir
         elif curr_node in weak_nodes:
             # No direction change
-            # print 'Step ', i + 1, 'In Weak Node, Infecting  ', curr_node, 'moving forward'
             weak_nodes.remove(curr_node)
             infected_nodes.add(curr_node)
             infections += 1
-            curr_node = (curr_node[0] + curr_dir[0], curr_node[1] + curr_dir[1])
         elif curr_node in flagged_nodes:
             # Reverses Direction
             next_dir = get_next_dir(curr_dir, 'reverse')
-            # print 'Step ', i + 1, 'In Flagged Node, Cleaning  ', curr_node, 'moving reverse'
             flagged_nodes.remove(curr_node)
-            curr_node = (curr_node[0] + next_dir[0], curr_node[1] + next_dir[1])
-            curr_dir = next_dir
         else:  # In Clean node
             next_dir = get_next_dir(curr_dir, 'left')
-            # print 'Step ', i + 1, 'In Clean Node, Weakening  ', curr_node, 'moving left'
             weak_nodes.add(curr_node)
-            curr_node = (curr_node[0] + next_dir[0], curr_node[1] + next_dir[1])
-            curr_dir = next_dir
+        curr_node = curr_node + next_dir
+        curr_dir = next_dir
     return infections
